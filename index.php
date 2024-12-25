edbfa95e51a0da7651077fb266987fcc2ce11a87
Jual Rana <contactjual@gmail.com>
2024-12-25 16:26:19 +0600

    some added and resolved

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ echo "Hello"; // echo does not use for retuning any value
 
 
 
-// more ------------------------------
+// more -------------echo va print-----------------
 
 echo "Hi"; // echo used for only showing
-print "Hi"; // it's also a vlaue and return 1. we can store it
+print "Hi"; // print also use for showing data. But it has a vlaue_ 1. we can store it. 
 
 $my_print_value = print "Hi"; // output: Hi
 echo "print output: " .$my_print_value; // output: 1
@@ -17,10 +17,17 @@ echo "print output: " .$my_print_value; // output: 1
 
 // echo ("Rahim","korim","korim"); // wrong: couse echo is not a funciton
 
-echo "Rahim","korim","korim"; // but echo can take multiple arguments
+echo "Rahim","korim","korim"; // but echo can take multiple parameters
 
-// print "Rahim","korim","korim"; // wrong: print cann't take multuple arguments
+// print "Rahim","korim","korim"; // wrong: print cann't take multuple arguments. only one argument can take.
 // print ("Rahim","korim","korim"); // wrong: print cann't take multuple arguments
+
+
+$x = 4;
+echo " price: $x"; // for print This process not true
+echo ' price: '.$x;  // only this true for print
+print ' price: $x'; // wrong
+print ' price: '.$x;
 
 
 
