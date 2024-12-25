5601cd4c317850fd5e7bc87d4dc15b925605c96f
Jual Rana <contactjual@gmail.com>
2024-12-25 13:15:34 +0600

    print vs echo

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+print "Hello"; // print means returning a value but echo is not that though output same
+echo "Hello"; // echo does not use for retuning any value
+
+
+
+
+// more ------------------------------
+
+echo "Hi"; // echo used for only showing
+print "Hi"; // it's also a vlaue and return 1. we can store it
+
+$my_print_value = print "Hi"; // output: Hi
+echo "print output: " .$my_print_value; // output: 1
+
+
+// echo ("Rahim","korim","korim"); // wrong: couse echo is not a funciton
+
+echo "Rahim","korim","korim"; // but echo can take multiple arguments
+
+// print "Rahim","korim","korim"; // wrong: print cann't take multuple arguments
+// print ("Rahim","korim","korim"); // wrong: print cann't take multuple arguments
+
+
+
+
+
+
+// html tag .......................................
+
+echo "<h1> Hi I am Jual </h1>";
+
+// echo "<h1 class= "text-color"> Hi I am color text </h1>"; // wrong code
+echo '<h1 class= "text-color"> Hi I am color text </h1>'; // right code
+echo "<h1 class= \"text-color\"> Hi I am color text </h1>"; // right code
+
+
+
+#09 to #13
+
+?>
